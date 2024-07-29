f4d38b6b339b9b5f9f8c9044ac2368b23f3c755c
murtazox04 <murtazox04@gmail.com>
2024-07-29 12:26:54 +0500

    Fix: install_requires in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='sqlalchemy_filters',
     version='0.1.0',
@@ -8,12 +11,13 @@ setup(
     install_requires=[
         'pydantic>=2.8.2',
         'SQLAlchemy>=2.0.31',
-        'starlette>=0.38.2',
+        'starlette>=0.37.2,<0.38.0',
     ],
+    python_requires='>=3.6',
     author='Murtazo Khurramov',
     author_email='murtazox04@gmail.com',
     description='A package for filtering SQLAlchemy queries.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/murtazox04/sqlalchemy-filters',
     classifiers=[
